c0c29537886531a7200385c46bb32763a386e7cc
Christian Fonseca <chrisgfwheller@hotmail.com>
2024-02-07 16:34:29 -0300

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { HeroSection } from "./components/HeroSection";
+
+export function App() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,28 @@
+import hero_section from "../assets/hero_section.svg";
+
+export function HeroSection() {
+  return (
+    <section className="flex flex-col">
+      <img src={hero_section} className="-z-10 absolute" alt="" />
+      <nav className="md:flex justify-between px-40 mt-10 h-14 items-center hidden text-white md:text-2xl bg-black bg-opacity-15 font-Karla ">
+        <a href="" className="">
+          Galeria
+        </a>
+        <a href="">Sobre nós</a>
+        <a href="">Feedback</a>
+        <a href="">Contato</a>
+      </nav>
+      <div className=" mt-16 md:mt-8 lg:mt-20 xl:mt-36 ml-52 md:ml-96 lg:pl-36 xl:pl-[360px] flex flex-col w-fit text-white ">
+        <h1 className="font-Julius text-lg md:text-5xl lg:text-6xl xl:text-7xl">
+          Cabana do
+        </h1>
+        <h2 className="font-Julius text-lg md:text-6xl lg:text-7xl xl:text-8xl">
+          Pórtico
+        </h2>
+        <h3 className="font-Karla text-xs md:text-xl lg:text-2xl xl:text-3xl ml-auto">
+          Gramado
+        </h3>
+      </div>
+    </section>
+  );
+}
