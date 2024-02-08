7851204069516ae507b4b937f18059e308d052f5
Christian Fonseca <chrisgfwheller@hotmail.com>
2024-02-08 19:10:56 -0300

    add: design

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,18 +10,27 @@ export function Contact() {
             Instagram
           </button>
         </a>
-        <a href="https://www.airbnb.com.br/rooms/53605809?source_impression_id=p3_1695736573_tDUNdktQjbqKh%2B3r" target="_blank">
+        <a
+          href="https://www.airbnb.com.br/rooms/53605809?source_impression_id=p3_1695736573_tDUNdktQjbqKh%2B3r"
+          target="_blank"
+        >
           <button className="bg-marromCabana-200 text-white w-48 h-12 text-sm rounded-full mb-4 font-Karla">
             Airbnb
           </button>
         </a>
-        <a href="https://api.whatsapp.com/send?phone=5551982252552" target="_blank">
+        <a
+          href="https://api.whatsapp.com/send?phone=5551982252552"
+          target="_blank"
+        >
           <button className="bg-marromCabana-200 text-white w-48 h-12 text-sm rounded-full mb-4 font-Karla">
             WhatsApp
           </button>
         </a>
       </div>
-      <h4 className="mt-20 font-Karla text-sm text-end mr-8 mb-10">Desenvolvido por @chrisfonseca_</h4>
+      <div className="flex justify-between mt-20 font-Karla text-sm mb-10 mx-8 items-center">
+        <h4>UX/UI Design por Ellie Tokki</h4>
+        <h4>Desenvolvido por @chrisfonseca_</h4>
+      </div>
     </section>
   );
 }
