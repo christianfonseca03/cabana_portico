46eba3110a62a215e26f520ba2b32ceccd4783ab
Christian Fonseca <chrisgfwheller@hotmail.com>
2024-03-25 17:57:19 -0300

    fix: mobile nav

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,47 @@
+"use client";
+
+import { useState } from "react";
 import hero_section from "../assets/hero_section.svg";
 
+import { AlignJustify } from "lucide-react";
+
 export function HeroSection() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu);
+  };
   return (
     <section className="flex flex-col sm:items-center md:items-stretch">
       <img src={hero_section} className="-z-10 absolute" alt="" />
+      <div
+        className={
+          openMenu
+            ? "bg-gradient-to-b from-marromCabana-400 via-marromCabana-300 to-white h-screen flex flex-col items-center pt-4 transition-all md:hidden"
+            : " flex flex-col items-end pt-2 pr-2 transition-all md:hidden"
+        }
+      >
+        <AlignJustify className="text-white" onClick={handleOpenMenu} />
+        <nav
+          className={
+            openMenu
+              ? "text-white flex flex-col mt-8 font-Karla gap-2 text-2xl"
+              : "hidden"
+          }
+        >
+          <a href="#Galeria" onClick={handleOpenMenu}>
+            Galeria
+          </a>
+          <a href="#Sobre" onClick={handleOpenMenu}>
+            Sobre nós
+          </a>
+          <a href="#Feedback" onClick={handleOpenMenu}>
+            Feedback
+          </a>
+          <a href="#Contato" onClick={handleOpenMenu}>
+            Contato
+          </a>
+        </nav>
+      </div>
       <nav className="md:flex justify-between px-40 mt-10 h-14 items-center hidden text-white md:text-2xl bg-black bg-opacity-10 font-Karla ">
         <a href="#Galeria" className="hover:text-neutral-200 transition-colors">
           Galeria
@@ -21,7 +59,7 @@ export function HeroSection() {
           Contato
         </a>
       </nav>
-      <div className=" mt-16 md:mt-8 lg:mt-20 xl:mt-36 ml-52 md:ml-96 lg:pl-36 xl:pl-[360px] flex flex-col w-fit text-white">
+      <div className=" mt-8 lg:mt-20 xl:mt-36 ml-52 md:ml-96 lg:pl-36 xl:pl-[360px] flex flex-col w-fit text-white">
         <h1 className="font-Julius text-lg md:text-5xl lg:text-6xl xl:text-7xl text-end">
           Cabana do
         </h1>
